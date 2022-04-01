217f852c63031fe090dd31d5b8ea562522a7fe00
Brian Callahan <bcallah@openbsd.org>
2022-03-31 20:38:35 -0400

    Now uses a sliding window.

--- O.c
+++ O.c
@@ -18,13 +18,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int
-xorq(const char *line)
-{
+struct peephole {
+	char *line1;
+	char *line2;
+	char *line3;
+};
 
-	if (!strncmp("\tmovq $0, %r", line, 12)) {
-		(void) fprintf(stdout, "\txorq %%r%c%c, %%r%c%c\n", line[12],
-		    line[13], line[12], line[13]);
+static char *
+xstrdup(const char *s)
+{
+	char *p;
+
+	if (s == NULL)
+		return NULL;
+
+	if ((p = strdup(s)) == NULL) {
+		(void) fputs("O: error: xstrdup failed\n", stderr);
+
+		exit(1);
+	}
+
+	return p;
+}
+
+static int
+xorq(struct peephole *window)
+{
+	char buf[32], r1a, r1b;
+
+	if (window->line1 == NULL)
+		return 0;
+
+	if (!strncmp("\tmovq $0, %r", window->line1, 12)) {
+		if (strlen(window->line1) != 15) {
+			(void) fputs("O: error: xorq failed\n", stderr);
+
+			exit(1);
+		}
+
+		r1a = window->line1[12];
+		r1b = window->line1[13];
+
+		(void) snprintf(buf, sizeof(buf), "\txorq %%r%c%c, %%r%c%c\n",
+		    r1a, r1b, r1a, r1b);
+
+		free(window->line1);
+		window->line1 = xstrdup(buf);
 
 		return 1;
 	}
@@ -33,12 +72,28 @@ xorq(const char *line)
 }
 
 static int
-xorl(const char *line)
+xorl(struct peephole *window)
 {
+	char buf[32], e1a, e1b;
 
-	if (!strncmp("\tmovl $0, %e", line, 12)) {
-		(void) fprintf(stdout, "\txorl %%e%c%c, %%e%c%c\n", line[12],
-		    line[13], line[12], line[13]);
+	if (window->line1 == NULL)
+		return 0;
+
+	if (!strncmp("\tmovl $0, %e", window->line1, 12)) {
+		if (strlen(window->line1) != 15) {
+			(void) fputs("O: error: xorl failed\n", stderr);
+
+			exit(1);
+		}
+
+		e1a = window->line1[12];
+		e1b = window->line1[13];
+
+		(void) snprintf(buf, sizeof(buf), "\txorl %%e%c%c, %%e%c%c\n",
+		    e1a, e1b, e1a, e1b);
+
+		free(window->line1);
+		window->line1 = xstrdup(buf);
 
 		return 1;
 	}
@@ -47,121 +102,140 @@ xorl(const char *line)
 }
 
 static void
-one(const char *line)
+one(struct peephole *window)
 {
 
-	if (xorq(line))
+	if (xorq(window))
 		return;
 
-	if (xorl(line))
+	if (xorl(window))
 		return;
-
-	(void) fputs(line, stdout);
 }
 
 static int
-mov(const char *line, FILE *fp)
+mov(struct peephole *window)
 {
-	char *line2 = NULL, *line3 = NULL;
+
+	if (window->line1 == NULL || window->line2 == NULL ||
+	    window->line3 == NULL) {
+		return 0;
+	}
+
+	if (strncmp("\tmovl %e", window->line1, 8) != 0 &&
+	    strncmp("\tmovq %r", window->line1, 8) != 0) {
+		return 0;
+	}
+
+	if (strncmp("\tmovl %e", window->line2, 8) != 0 &&
+	    strncmp("\tmovq %r", window->line2, 8) != 0) {
+		return 0;
+	}
+
+	if (strncmp("\tmovl %e", window->line3, 8) != 0 &&
+	    strncmp("\tmovq %r", window->line3, 8) != 0 &&
+	    strncmp("\tmovl $0, %e", window->line3, 12) != 0 &&
+	    strncmp("\tmovq $0, %r", window->line3, 12) != 0) {
+		return 0;
+	}
+
+	if (window->line1[4] == window->line2[4] &&
+	    window->line1[7] == window->line2[13] &&
+	    window->line1[8] == window->line2[14] &&
+	    window->line1[9] == window->line2[15] &&
+	    window->line1[13] == window->line2[7] &&
+	    window->line1[14] == window->line2[8] &&
+	    window->line1[15] == window->line2[9] &&
+	    window->line1[14] == window->line3[strlen(window->line3) - 3] &&
+	    window->line1[15] == window->line3[strlen(window->line3) - 2]) {
+		free(window->line2);
+		window->line2 = NULL;
+
+		free(window->line1);
+		window->line1 = xstrdup(window->line3);
+
+		free(window->line3);
+		window->line3 = NULL;
+
+		return 1;
+	}
+
+	return 0;
+}
+
+static int
+three(struct peephole *window)
+{
+
+	return mov(window);
+}
+
+static int
+fillwindow(struct peephole *window, FILE *fp)
+{
 	size_t size = 0;
 
-	if (strncmp("\tmovl %e", line, 8) != 0 &&
-	    strncmp("\tmovq %r", line, 8) != 0) {
-		one(line);
+	if (window->line1 == NULL) {
+		if (getline(&window->line1, &size, fp) == -1)
+			return 0;
+	}
 
+	if (window->line2 == NULL) {
+		if (getline(&window->line2, &size, fp) == -1)
+			return 0;
+	}
+
+	if (getline(&window->line3, &size, fp) == -1)
 		return 0;
-	}
 
-	if (getline(&line2, &size, fp) == -1) {
-		(void) fputs(line, stdout);
-
-		return 1;
-	}
-
-	if (strncmp("\tmovl %e", line2, 8) != 0 &&
-	    strncmp("\tmovq %r", line2, 8) != 0) {
-		(void) fputs(line, stdout);
-		one(line2);
-
-		free(line2);
-
-		return 0;
-	}
-
-	if (getline(&line3, &size, fp) == -1) {
-		(void) fputs(line, stdout);
-		(void) fputs(line2, stdout);
-
-		free(line2);
-
-		return 1;
-	}
-
-	if (strncmp("\tmovl", line3, 5) != 0 &&
-	    strncmp("\tmovq", line3, 5) != 0) {
-		(void) fputs(line, stdout);
-		(void) fputs(line2, stdout);
-		one(line3);
-
-		free(line3);
-		free(line2);
-
-		return 0;
-	}
-
-	if (line[5] == line2[5] &&
-	    line[7] == line2[13] &&
-	    line[8] == line2[14] &&
-	    line[9] == line2[15] &&
-	    line[13] == line2[7] &&
-	    line[14] == line2[8] &&
-	    line[15] == line2[9] &&
-	    line[14] == line3[14] &&
-	    line[15] == line3[15]) {
-		(void) fputs(line3, stdout);
-	} else {
-		(void) fputs(line, stdout);
-		(void) fputs(line2, stdout);
-		one(line3);
-	}
-
-	free(line3);
-	free(line2);
-
-	return 0;
+	return 1;
 }
 
-static int
-three(const char *line, FILE *fp)
+static void
+shiftwindow(struct peephole *window)
 {
 
-	if (mov(line, fp) == 1)
-		return 1;
+	free(window->line1);
+	window->line1 = xstrdup(window->line2);
 
-	return 0;
+	free(window->line2);
+	window->line2 = xstrdup(window->line3);
 }
 
 static void
 O(FILE *fp)
 {
-	char *line = NULL;
-	size_t size = 0;
+	struct peephole window;
+	int ret;
 
-	while (getline(&line, &size, fp) != -1) {
-		if (three(line, fp) == 1)
-			break;
+	window.line1 = NULL;
+	window.line2 = NULL;
+	window.line3 = NULL;
+
+	while (fillwindow(&window, fp)) {
+again:
+		ret = three(&window);
+		one(&window);
+
+		if (ret == 1) {
+			if (fillwindow(&window, fp))
+				goto again;
+		}
+
+		(void) fputs(window.line1, stdout);
+
+		shiftwindow(&window);
 	}
 
-	free(line);
-}
+	free(window.line3);
+	window.line3 = NULL;
 
-static int
-usage(void)
-{
+	while (window.line1 != NULL) {
+		one(&window);
 
-	(void) fputs("usage: O in.s [-o out.s]\n", stderr);
+		(void) fputs(window.line1, stdout);
 
-	return 1;
+		shiftwindow(&window);
+	}
 }
 
 int
@@ -171,14 +245,17 @@ main(int argc, char *argv[])
 
 	if (argc == 4) {
 		if (strcmp(argv[2], "-o") != 0)
-			return usage();
+			goto usage;
 
 		if (freopen(argv[3], "w+", stdout) == NULL) {
 			(void) fprintf(stderr, "O: error: couldn't open %s\n",
 			    argv[3]);
 		}
 	} else if (argc != 2) {
-		return usage();
+usage:
+		(void) fputs("usage: O in.s [-o out.s]\n", stderr);
+
+		return 1;
 	}
 
 	if (!strcmp(argv[1], "-")) {
