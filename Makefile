4214af9476098f0d0523eedd58fef9769a2754c9
Brian Callahan <ibara@users.noreply.github.com>
2025-08-31 20:27:46 -0400

    Add first pass at arm64

--- Makefile
+++ Makefile
@@ -8,11 +8,19 @@ CFLAGS ?=	-g -O2
 PROG =	O
 OBJS =	O.o
 
-all: ${OBJS}
-	${CC} ${LDFLAGS} -o ${PROG} ${OBJS}
+ARM64 =	arm64.o
+X64 =	x64.o
+
+all: arm64 x64
+
+arm64: ${OBJS} ${ARM64}
+	${CC} ${LDFLAGS} -o ${PROG}.$@ ${OBJS} ${ARM64}
+
+x64: ${OBJS} ${X64}
+	${CC} ${LDFLAGS} -o ${PROG}.$@ ${OBJS} ${X64}
 
 install:
 	install -c -S -s -m 755 ${PROG} ${PREFIX}/bin
 
 clean:
-	rm -f ${PROG} ${OBJS} ${PROG}.core
+	rm -f ${PROG}.* ${OBJS} ${ARM64} ${X64} *.core
---
+++ arm64.c
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2022, 2025 Brian Callahan <bcallah@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "O.h"
+
+static void
+one(struct peephole *window)
+{
+}
+
+static int
+mov(struct peephole *window)
+{
+	int c, i = 0, j = 0;
+	char r1[4], r2[4], r3[4], r4[4];
+
+	if (window->line1 == NULL || window->line2 == NULL)
+		return 0;
+
+	if (strncmp("\tmov\tx", window->line1, 6) != 0 ||
+	    strncmp("\tmov\tx", window->line2, 6) != 0) {
+			return 0;
+	}
+
+	(void) memset(r1, 0, 4);
+
+	c = window->line1[i + 6];
+
+	while (c != ',' && c != ' ' && c != '\n') {
+		if (i == 3)
+			return 0;
+		r1[i++] = c;
+		c = window->line1[i + 6];
+	}
+
+	while (c == ',' || c == ' ') {
+		c = window->line1[i + 6];
+		if (c == '\n')
+			return 0;
+		++i;
+	}
+
+	(void) memset(r2, 0, 4);
+
+    while (c != ',' && c != ' ' && c != '\n') {
+		if (j == 3)
+			return 0;
+		r2[j++] = c;
+		c = window->line1[i + 6];
+		++i;
+	}
+
+	if (c != '\n')
+		return 0;
+
+	i = 0;
+	(void) memset(r3, 0, 4);
+
+	c = window->line2[i + 6];
+
+	while (c != ',' && c != ' ' && c != '\n') {
+		if (i == 3)
+			return 0;
+		r3[i++] = c;
+		c = window->line2[i + 6];
+	}
+
+	while (c == ',' || c == ' ') {
+		c = window->line2[i + 6];
+		if (c == '\n')
+			return 0;
+		++i;
+	}
+
+	j = 0;
+
+    while (c != ',' && c != ' ' && c != '\n') {
+		if (j == 3)
+			return 0;
+		r1[j++] = c;
+		c = window->line2[i + 6];
+		++i;
+	}
+
+	if (c != '\n')
+		return 0;
+
+	if (r1[0] != 'x' || r2[0] != 'x' || r3[0] != 'x' || r4[0] != 'x')
+		return 0;
+
+	if (strcmp(r1, r4) != 0 || strcmp(r2, r3) != 0)
+		return 0;
+
+	free(window->line2);
+	window->line2 = xstrdup(window->line3);
+
+	free(window->line3);
+	window->line3 = NULL;
+
+	return 1;
+}
+
+static int
+two(struct peephole *window)
+{
+
+	return mov(window);
+}
+
+static int
+three(struct peephole *window)
+{
+
+	return 0;
+}
+
+void
+arm64(FILE *fp)
+{
+	struct peephole window;
+	int ret;
+
+	window.line1 = NULL;
+	window.line2 = NULL;
+	window.line3 = NULL;
+
+	while (fillwindow(&window, fp)) {
+again:
+		ret = three(&window);
+		if (ret == 0)
+			ret = two(&window);
+		one(&window);
+
+		if (ret == 1) {
+			if (fillwindow(&window, fp))
+				goto again;
+		}
+
+		if (window.line1 != NULL)
+			(void) fputs(window.line1, stdout);
+
+		shiftwindow(&window);
+	}
+
+	free(window.line3);
+	window.line3 = NULL;
+
+	while (window.line1 != NULL) {
+		one(&window);
+
+		if (window.line1 != NULL)
+			(void) fputs(window.line1, stdout);
+
+		shiftwindow(&window);
+	}
+}
